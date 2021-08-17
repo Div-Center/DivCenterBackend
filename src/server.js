2a4c870b6811e9a3b955f2aee78e93e30e28fe03
Tyler Johnson <tyler@csdconsultants.us>
2021-08-17 12:45:13 -0700

    adds services route

--- src/server.js
+++ src/server.js
@@ -3,21 +3,16 @@
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require('./auth/routes')
-const carRoutes = require('./routes/car.js');
-const foodRoutes = require('./routes/food.js');
-const todoRoutes = require('./routes/todo')
-const userRoutes = require('./routes/user')
+const servicesRoutes = require('./routes/services.js')
+const userRoutes = require('./routes/user.js')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(authRoutes);
-app.use(carRoutes);
-app.use(foodRoutes);
-app.use(todoRoutes);
+
+app.use(servicesRoutes);
 app.use(userRoutes);
 
 module.exports = {
