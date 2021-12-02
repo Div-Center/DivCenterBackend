62435960681da43772f0e72876fa13643e62b5b8
taegorov <taegorov@gmail.com>
2021-12-01 16:51:09 -0800

    changes DB to ElephantSQL cloud DB

--- src/models/index.js
+++ src/models/index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 // const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 // const DATABASE_URL = 'postgres://postgres@localhost:5432';
 
-// below: throw a !== instead of === if you want to connect to real database
-const DATABASE_URL = process.env.NODE_ENV === 'production' ? process.env.DATABASE_PROD : process.env.DATABASE_DEV
+// below: throw a !== instead of === if you want to connect to ElephantSQL database (instead of local)
+const DATABASE_URL = process.env.NODE_ENV !== 'production' ? process.env.DATABASE_PROD : process.env.DATABASE_DEV
 
 const { Sequelize, DataTypes } = require('sequelize');
 
